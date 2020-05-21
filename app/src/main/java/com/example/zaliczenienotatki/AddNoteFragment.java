32faa914f9a77a745183b921d608ff70b2ee1b68
JanGrabara <jangrabara@gmail.com>
2020-05-21 17:45:19 +0200

    basic fragment

---
+++ app/src/main/java/com/example/zaliczenienotatki/AddNoteFragment.java
@@ -0,0 +1,37 @@
+package com.example.zaliczenienotatki;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddNoteFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddNoteFragment extends Fragment {
+
+    private AppDatabase db;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_note, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        db = AppDatabase.getInstance(getContext());
+    }
+}
---
+++ app/src/main/java/com/example/zaliczenienotatki/FragmentAdapter.java
@@ -0,0 +1,36 @@
+package com.example.zaliczenienotatki;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+class FragmentAdapter extends FragmentStatePagerAdapter {
+
+    FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        if(position==0)
+            return new AddNoteFragment();
+        return new AddNoteFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position==0){
+            return "Dodaj notatke";
+        }
+        return "Wykres";
+    }
+}
